bca36a53ffefa2cfdd46ceacae8fafce39768337
Patrick Sørensen <10461259@ucn.dk>
2023-03-31 12:24:08 +0200

    Added getters & setters

--- src/model/Instructor.java
+++ src/model/Instructor.java
@@ -1,5 +1,18 @@
 package model;
 
 public class Instructor extends Person{
-private String subject;
+    private String subject;
+
+    public Instructor(String name, String address, String email, String phoneNumber, int role, String username, String password, int ssn, String subject) {
+        super(name, address, email, phoneNumber, role, username, password, ssn);
+        this.subject = subject;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
 }
