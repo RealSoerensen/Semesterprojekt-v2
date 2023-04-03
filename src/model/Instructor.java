2d5ef3cac8b34f57d64f948b09122f9df0ef585e
SvendBD <115786653+SvendBD@users.noreply.github.com>
2023-04-03 11:06:51 +0200

    DAL Ændringer
    Tilføjet til DAL Create Member og små ændringer i Person og member

--- src/model/Instructor.java
+++ src/model/Instructor.java
@@ -3,8 +3,8 @@ package model;
 public class Instructor extends Person{
     private String subject;
 
-    public Instructor(String name, Address address, String email, String phoneNumber, int role, String username, String password, int ssn, String subject) {
-        super(name, address, email, phoneNumber, role, username, password, ssn);
+    public Instructor(String firstName, String lastName, Address address, String email, String phoneNumber, int role, String username, String password, int ssn, String subject) {
+        super(firstName, lastName, address, email, phoneNumber, role, username, password, ssn);
         this.subject = subject;
     }
 
