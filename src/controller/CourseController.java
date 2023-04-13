8eb9b7fd4cb5fa3095a0a8ff73553b9043443ca7
SvendBD <115786653+SvendBD@users.noreply.github.com>
2023-04-13 10:34:04 +0200

    Mark absent usecase

--- src/controller/CourseController.java
+++ src/controller/CourseController.java
@@ -1,5 +1,49 @@
 package controller;
 
-public class CourseController {
+import java.sql.SQLException;
 
+import dal.*;
+import model.*;
+
+public class CourseController {
+	private CourseDB courseDB;
+	private CourseSessionDB courseSessionDB;
+	private PersonController pc;
+	
+	public CourseController() throws SQLException {
+		courseDB = new CourseDB();
+		courseSessionDB = new CourseSessionDB();
+		pc = new PersonController();
+	}
+	
+	public void markAbsent(long courseSessionID, long ssn) throws SQLException {
+		CourseSession courseSession = courseSessionDB.get(courseSessionID);
+		Person personToBeMarkedAbsent = null;
+		
+		int role = pc.getRoleOfPerson(ssn);
+		if(role == 1) {
+			personToBeMarkedAbsent = getMemberFromCourseSession(ssn, courseSession);
+		}
+		else if(role == 2) {
+			personToBeMarkedAbsent = courseSession.getInstructor();
+		}
+		
+		//TODO Fjerne en person fra courseSession baseret på courseSessionID og ssn
+	}
+	
+	private Member getMemberFromCourseSession(long ssn, CourseSession courseSession) {
+		Member member = null;
+
+		int amountOfMembers = courseSession.getMembers().size();
+		boolean found = false;
+		while(amountOfMembers > 0 && !found) {
+			if(ssn == courseSession.getMembers().get(amountOfMembers).getSsn()) {
+				member = courseSession.getMembers().get(amountOfMembers);
+				found = true;
+			}
+			amountOfMembers--;
+		}
+		
+		return member;
+	}
 }
--- src/controller/PersonController.java
+++ src/controller/PersonController.java
@@ -1,7 +1,18 @@
 package controller;
 
+import java.sql.SQLException;
+
+import dal.*;
+
 public class PersonController {
 
+	private PersonDB personDB;
 	
+	public PersonController() throws SQLException {
+		personDB = new PersonDB();
+	}
 	
+	public int getRoleOfPerson(long ssn) {
+		return personDB.get(ssn).getRole();
+	}
 }
