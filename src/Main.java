e53417395734a9fc41e20f6c73011b796f7adea4
Birch2000 <jonas.birch00@gmail.com>
2023-05-15 13:57:19 +0200

    Main update
    Persosn added to the systems

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.sql.SQLException;
+
+import controller.PersonController;
+import dal.address.AddressContainer;
 import dal.person.PersonContainer;
 import gui.LoginMenu;
 import gui.MainMenu;
@@ -6,18 +10,51 @@ import model.Person;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		
 		//Create Addresses
 		Address address1 = new Address("9000", "Aalborg", "Her", "5A");
+		Address address2 = new Address("9000", "Aalborg", "Der", "1");
+		Address address3 = new Address("9220", "Aalborg", "Hvorhenne", "73");
+		Address address4 = new Address("9200", "Aalborg", "Bilka", "252");
+		Address address5 = new Address("9000", "Aalborg", "Langbortistan", "3523");
+		Address address6 = new Address("8900", "Randers", "Storegade", "17");
+		Address address7 = new Address("9000", "Aalborg", "Jomfru Ane Gade", "23");
+		Address address8 = new Address("9000", "Aalborg", "Papkasse", "3");
+		Address address9 = new Address("9000", "Aalbrog", "Jomfru Ane Gade", "5");
+		Address address10 = new Address("9200", "Aalborg", "Sofiendalsvej", "60");
+		
 		
 		
 		//Create members, instructors and administrators
-		PersonContainer personContainer = PersonContainer.getInstance();
-		Person person1 = new Person("Patrick", "Sørensen", address1, "UwULover@gmail.com", "112", 3, "0", 111);
-		Person person2 = new Person("Jonas", "Jørgensen", address1, "Jonas.birch00@gmail.com", "51408590", 3, "1", 222);
-		personContainer.create(person1);
-		personContainer.create(person2);
+		PersonController personController = new PersonController(PersonContainer.getInstance(), AddressContainer.getInstance());
+		
+		Person person1 = new Person("Patrick", "Sørensen", address1, "UwULover@gmail.com", "112", 3, "uwu", "0", 1111);
+		Person person2 = new Person("Jonas", "Jørgensen", address2, "Jonas.birch00@gmail.com", "51408590", 3, "Birch", "1", 9999);
+		Person person3 = new Person("Svend", "Damsgaard", address3, "svendmaster@gmail.com", "11111111", 3, "Svend", "0", 3333);
+		
+		Person person4 = new Person("Filip", "Nymann", address4, "filipnymann@hotmail.com", "88888888", 2, "Filip", "2", 7777);
+		Person person5 = new Person("Nicklas", "Aagaard", address5, "nicklasspillerkata@hotmail.com", "25252525", 2, "Nicklas", "2", 5555);
+		
+		Person person6 = new Person("Britta", "Mokaisen", address6, "mokaidrikker@gmail.com", "11411414", 1, "Mokai", "0", 9878);
+		Person person7 = new Person("Jannik", "Bøllesen", address7, "nordjyde123@live.dk", "69646567", 1, "Brød", "0", 5687);
+		Person person8 = new Person("Mogens", "Hansen", address8, "hjemløsherre1525@gmail.com", "11223344", 1, "Homelessdude", "0", 5547);
+		Person person9 = new Person("Bente", "Mortensen", address9, "mortensen.bente@hotmail.com", "99887766", 1, "Bente", "0", 6658);
+		Person person10 = new Person("Ollie", "Osbourne", address10, "osbourne@live.dk", "55566688", 1, "Osbourne", "0", 6659);
+		
+		
+		personController.createPerson(person1);
+		personController.createPerson(person2);
+		personController.createPerson(person3);
+		personController.createPerson(person4);
+		personController.createPerson(person5);
+		personController.createPerson(person6);
+		personController.createPerson(person7);
+		personController.createPerson(person8);
+		personController.createPerson(person9);
+		personController.createPerson(person10);
+		
+
 		new LoginMenu().run();
 		
 	}
--- src/gui/EditAccountMenu.java
+++ src/gui/EditAccountMenu.java
@@ -33,7 +33,7 @@ public class EditAccountMenu extends JFrame {
 	public EditAccountMenu(Person person) {
 		setName("Bruh");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 829, 670);
+		setBounds(100, 100, 634, 548);
 		contentPanel = new JPanel();
 
 		setContentPane(contentPanel);
@@ -43,104 +43,104 @@ public class EditAccountMenu extends JFrame {
 		lblYourInformation.setHorizontalAlignment(SwingConstants.CENTER);
 		lblYourInformation.setFont(new Font("Tahoma", Font.PLAIN, 30));
 		lblYourInformation.setBounds(10, 11, 600, 39);
-		add(lblYourInformation);
+		getContentPane().add(lblYourInformation);
 
 		JSeparator separator = new JSeparator();
 		separator.setBounds(10, 61, 600, 2);
-		add(separator);
+		getContentPane().add(separator);
 		JSeparator separator_1 = new JSeparator();
 		separator_1.setOrientation(SwingConstants.VERTICAL);
-		separator_1.setBounds(310, 82, 2, 500);
-		add(separator_1);
+		separator_1.setBounds(310, 82, 2, 347);
+		getContentPane().add(separator_1);
 		JSeparator separator_2 = new JSeparator();
 		separator_2.setBounds(10, 427, 600, 2);
-		add(separator_2);
+		getContentPane().add(separator_2);
 
 
 		
 		JLabel lblFirstNameText = new JLabel("Fornavn:");
 		lblFirstNameText.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblFirstNameText.setBounds(10, 82, 50, 20);
-		add(lblFirstNameText);
+		lblFirstNameText.setBounds(10, 82, 78, 20);
+		getContentPane().add(lblFirstNameText);
 
 		JTextField txtFirstName = new JTextField(person.getFirstName());
 		txtFirstName.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		txtFirstName.setBounds(70, 82, 231, 20);
-		add(txtFirstName);
+		txtFirstName.setBounds(86, 82, 215, 20);
+		getContentPane().add(txtFirstName);
 
 		JLabel lblLastName = new JLabel("Efternavn:");
 		lblLastName.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblLastName.setBounds(10, 113, 60, 20);
-		add(lblLastName);
+		lblLastName.setBounds(10, 113, 78, 20);
+		getContentPane().add(lblLastName);
 
 		JTextField txtLastName = new JTextField(person.getLastName());
 		txtLastName.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		txtLastName.setBounds(70, 113, 231, 20);
-		add(txtLastName);
+		txtLastName.setBounds(86, 113, 215, 20);
+		getContentPane().add(txtLastName);
 
 		JButton btnEditName = new JButton("Rediger Navn");
 		btnEditName.addActionListener(e -> {
 			person.setFirstName(txtFirstName.getText());
 			person.setLastName(txtLastName.getText());
 		});
-		btnEditName.setBounds(70, 144, 231, 23);
-		add(btnEditName);
+		btnEditName.setBounds(86, 144, 215, 23);
+		getContentPane().add(btnEditName);
 		
 		JLabel lblEmailText = new JLabel("Email:");
 		lblEmailText.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblEmailText.setBounds(10, 215, 50, 20);
-		add(lblEmailText);
+		getContentPane().add(lblEmailText);
 
 		JTextField txtEmail = new JTextField(person.getEmail());
 		txtEmail.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		txtEmail.setBounds(70, 215, 231, 20);
-		add(txtEmail);
+		txtEmail.setBounds(86, 215, 215, 20);
+		getContentPane().add(txtEmail);
 
 		JButton btnEditEmail = new JButton("Rediger Email");
 		btnEditEmail.addActionListener(e -> person.setEmail(txtEmail.getText()));
-		btnEditEmail.setBounds(10, 246, 131, 39);
-		add(btnEditEmail);
+		btnEditEmail.setBounds(86, 246, 214, 23);
+		getContentPane().add(btnEditEmail);
 		
 		JLabel lblPhoneText = new JLabel("Telefon:");
 		lblPhoneText.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblPhoneText.setBounds(10, 333, 50, 20);
-		add(lblPhoneText);
+		lblPhoneText.setBounds(10, 333, 66, 20);
+		getContentPane().add(lblPhoneText);
 
 		JTextField txtPhone = new JTextField(person.getPhoneNumber());
 		txtPhone.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		txtPhone.setBounds(70, 333, 231, 20);
-		add(txtPhone);
+		txtPhone.setBounds(86, 333, 215, 20);
+		getContentPane().add(txtPhone);
 
 		JButton btnEditPhone = new JButton("Rediger Telefon");
 		btnEditPhone.addActionListener(e -> person.setPhoneNumber(txtPhone.getText()));
-		btnEditPhone.setBounds(10, 364, 131, 39);
-		add(btnEditPhone);
+		btnEditPhone.setBounds(86, 364, 214, 23);
+		getContentPane().add(btnEditPhone);
 
 		JLabel lblAddressText = new JLabel("Adresse");
 		lblAddressText.setFont(new Font("Tahoma", Font.PLAIN, 25));
 		lblAddressText.setBounds(323, 82, 285, 39);
-		add(lblAddressText);
+		getContentPane().add(lblAddressText);
 
 		JTextField txtPostalCode = new JTextField(person.getAddress().getZipCode());
 		txtPostalCode.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		txtPostalCode.setBounds(323, 171, 285, 20);
-		add(txtPostalCode);
+		getContentPane().add(txtPostalCode);
 
 
 		JTextField txtCity = new JTextField(person.getAddress().getCity());
 		txtCity.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		txtCity.setBounds(323, 202, 285, 20);
-		add(txtCity);
+		getContentPane().add(txtCity);
 
 		JTextField txtRoadname = new JTextField(person.getAddress().getStreet());
 		txtRoadname.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		txtRoadname.setBounds(323, 233, 285, 20);
-		add(txtRoadname);
+		getContentPane().add(txtRoadname);
 
 		JTextField txtHouseNumber = new JTextField(person.getAddress().getHouseNumber());
 		txtHouseNumber.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		txtHouseNumber.setBounds(323, 264, 285, 20);
-		add(txtHouseNumber);
+		getContentPane().add(txtHouseNumber);
 
 		JButton btnEditAddress = new JButton("Rediger Adresse");
 		btnEditAddress.addActionListener(e -> {
@@ -150,7 +150,7 @@ public class EditAccountMenu extends JFrame {
 			person.getAddress().setHouseNumber(txtHouseNumber.getText());
 		});
 		btnEditAddress.setBounds(323, 295, 285, 23);
-		add(btnEditAddress);
+		getContentPane().add(btnEditAddress);
 		
 		JButton btnDoneEditInfo = new JButton("Færdiggør Redigering");
 		btnDoneEditInfo.addActionListener(e -> {
@@ -164,6 +164,6 @@ public class EditAccountMenu extends JFrame {
 			frame.dispose();
 		});
 		btnDoneEditInfo.setBounds(10, 440, 600, 39);
-		add(btnDoneEditInfo);
+		getContentPane().add(btnDoneEditInfo);
 	}
 }
